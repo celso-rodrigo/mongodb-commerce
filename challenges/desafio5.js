daae6bcec0e20eea91dbda8a5cbc892100d1e662
celsorodrigo <celsorodrigoma@gmail.com>
2022-12-15 15:05:13 -0300

    Concluído 6° requisito; Melhorada identação dos requisitos.

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,11 @@
 db.produtos.find(
-  { $or: [{ curtidas: 36 }, { vendidos: 85 }] },
-  { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
+  {
+     $or: [{ curtidas: 36 }, { vendidos: 85 }],
+  },
+  { 
+    _id: 0,
+    nome: 1,
+    curtidas: 1,
+    vendidos: 1,
+  },
 );
